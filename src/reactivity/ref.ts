d6169d855d8674893ecb6c0b7509a161b40c84e9
unknown <552410301@qq.com>
2024-04-20 20:25:29 +0800

    '新增proxyRefs方法'

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -38,3 +38,19 @@ export function isRef(raw: any) {
 export function unRef(raw: any) {
   return isRef(raw) ? raw.value : raw;
 }
+export function proxyRefs(raw: any) {
+    //作用：获取属性值的时候不需要用.value;
+  return new Proxy(raw, {
+    get(target, key) {
+      return unRef(Reflect.get(target, key));
+    },
+    set(target, key, newVal) {
+      if (isRef(target[key]) && !isRef(newVal)) {
+        //如果原来是ref类型，新的值是普通类型
+        return (target[key].value = newVal);
+      } else {
+        return Reflect.set(target, key, newVal);
+      }
+    },
+  });
+}
